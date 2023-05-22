33e99aa5dbed16e5b8369a18ad8b81e9d5c19907
z9mb1 <77471503+sij411@users.noreply.github.com>
2023-05-22 23:59:55 +0900

    html structure

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # toDoWebApp-2023-may
  
+#### 
+
+##### 5-22-2023 
+# 1. todolist html
+# 2. function 
+# 2-1. add lists
+# 2-2. del lists
---
+++ app.js
@@ -0,0 +1,13 @@
+
+const cnt = 0;
+
+function tempNameGenerator (tag) {
+    
+    cnt += 1;
+
+    var g = document.getElementById(tag).innerHTML;
+
+    g = document.write("<input type='text'>");
+
+}
+}
