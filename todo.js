bf58ee1d2aad970d8bc5af58f39d6d142707621c
z9mb1 <77471503+sij411@users.noreply.github.com>
2023-05-31 10:51:58 +0900

    todo-list : add
    add newly typed text and create li

--- todo.js
+++ todo.js
@@ -1,16 +1,20 @@
-//input에 enter를 누르면 새로고침됨 -> 막고 싶음
-//메모를 적음 -> 엔터 누름 -> 값이 저장되고 인풋 칸은 비워짐.
-
+//input에 enter를 누르면 새로고침됨 -> 막고 싶음b -> solved.
+//메모를 적음 -> 엔터 누름 -> 값이 저장되고 인풋 칸은 비워짐. -> solved.
+// add li with value newToDo
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = document.getElementById("b");
+const toDoInput = document.querySelector("#todo-form input");
 
 function toDoEvent(event) {
   event.preventDefault();
-  console.log("working?");
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
+  console.log("This is " + newToDo);
+
+  var newLi = document.createElement("li");
+  var newText = document.createTextNode(newToDo);
+  newLi.appendChild(newText);
+  var currentUl = document.getElementById("todo-list");
+  currentUl.insertAdjacentElement("afterbegin", newLi);
 }
 
-toDoForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-});
-
-// event default 값이 안변함.
+toDoForm.addEventListener("submit", toDoEvent);
